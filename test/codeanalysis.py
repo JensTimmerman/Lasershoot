cbf57d1b91d57f3e2e25e245902f3d225f0340af
Jens Timmerman <jens.timmerman@gmail.com>
2018-03-21 20:51:30 +0100

    try to import each package

--- test/codeanalysis.py
+++ test/codeanalysis.py
@@ -25,8 +25,44 @@ class AnalyseTest(TestCase):
         sys.argv.extend(PROSPECTOR_OPTIONS)
         sys.argv.append(REPO_BASE_DIR)
         config = ProspectorConfig()
-        config.libraries = []
         prospector = Prospector(config)
         prospector.execute()
         failures = [msg.as_dict() for msg in prospector.get_messages()]
         self.assertFalse(failures, "prospector failures: %s" % pprint.pformat(failures))
+
+    def _import(self, pkg):
+        try:
+            __import__(pkg)
+        except ImportError as e:
+            log.debug("__path__ %s",
+                      ["%s = %s" % (name, getattr(mod, '__path__', 'None')) for name, mod in sys.modules.items()])
+            self.assertFalse(e, msg="import %s failed sys.path %s exception %s" % (pkg, sys.path, e))
+
+        self.assertTrue(pkg in sys.modules, msg='%s in sys.modules after import' % pkg)
+
+    def test_import_packages(self):
+        """Try to import each namespace"""
+        res = {'packages': {}, 'modules': {}}
+        offset = len(self.REPO_LIB_DIR.split(os.path.sep))
+        for root, _, files in os.walk(self.REPO_LIB_DIR):
+            package = '.'.join(root.split(os.path.sep)[offset:])
+            if '__init__.py' in files or package in excluded_pkgs:
+                # Force vsc shared packages/namespace
+                if '__init__.py' in files and (package == 'vsc' or package.startswith('vsc.')):
+                    init = open(os.path.join(root, '__init__.py')).read()
+                    if not re.search(r'^import\s+pkg_resources\npkg_resources.declare_namespace\(__name__\)$',
+                                     init, re.M):
+                        raise Exception(('vsc namespace packages do not allow non-shared namespace in dir %s.'
+                                         'Fix with pkg_resources.declare_namespace') % root)
+
+                res['packages'][package] = self.rel_gitignore([os.path.join(root, f) for f in files])
+
+                # this is a package, all .py files are modules
+                for mod_fn in res['packages'][package]:
+                    if not mod_fn.endswith('.py') or mod_fn.endswith('__init__.py'):
+                        continue
+                    modname = os.path.basename(mod_fn)[:-len('.py')]
+                    res['modules']["%s.%s" % (package, modname)] = mod_fn
+
+        for pkg in  res['packages']:
+            self._import(pkg)
